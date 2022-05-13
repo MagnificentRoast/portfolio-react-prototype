3d2249849098e45aae1405cb5a609f577e42e691
MagnificentRoast <94555704+MagnificentRoast@users.noreply.github.com>
2022-05-12 22:48:35 -0700

    added functional contact information in the footer.js file

--- portfolio-react-prototype/src/App.js
+++ portfolio-react-prototype/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/resume";
+import Nav from "./components/Nav"
+import Footer from "./components/Footer";
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Nav
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}>
+        </Nav>
       </header>
+      <main>
+        {renderPage()}
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
