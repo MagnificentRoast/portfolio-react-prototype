d60a23a3d4fe48002e8cae6d47a8c5f6b49c6fbd
MagnificentRoast <94555704+MagnificentRoast@users.noreply.github.com>
2022-05-12 22:32:34 -0700

    deleted modal because it felt unnecessary, also finished the footer index.js file

---
+++ portfolio-react-prototype/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+        <div>
+            <div>
+                <a href="https://github.com/MagnificentRoast">GitHub</a>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/drew-harris-326202133/">LinkedIn</a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
