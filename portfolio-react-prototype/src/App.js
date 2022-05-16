fb5b0b9c5a72b90560ffd03dc3a496a136151b20
MagnificentRoast <94555704+MagnificentRoast@users.noreply.github.com>
2022-05-15 21:44:31 -0700

    attempted to fix the code in app.js to no avail, I'll have to fix this

--- portfolio-react-prototype/src/App.js
+++ portfolio-react-prototype/src/App.js
@@ -1,37 +1,89 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import './App.css'
+// import About from "./components/About";
+// import Portfolio from "./components/Portfolio";
+// import Contact from "./components/Contact";
+// import Resume from "./components/resume";
+// import Nav from "./components/Nav"
+// import Footer from "./components/Footer";
+
+// function App() {
+
+//   const [currentPage, setCurrentPage] = useState("About");
+
+//   const renderPage = () => {
+//     if (currentPage === "About") {
+//       return <About />;
+//     }
+//     if (currentPage === "Portfolio") {
+//       return <Portfolio />;
+//     }
+//     if (currentPage === "Contact") {
+//       return <Contact />;
+//     }
+//     return <Resume />;
+//   };
+
+//   const handlePageChange = (page) => setCurrentPage(page);
+
+//   return (
+//     <div className="App">
+//       <header>
+//         <Nav
+//         currentPage={currentPage}
+//         handlePageChange={handlePageChange}>
+//         </Nav>
+//       </header>
+//       <main>
+//         {renderPage()}
+//       </main>
+//       <Footer></Footer>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// updated code using original react app code
+
+import React, { useState } from "react";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
-import Resume from "./components/resume";
-import Nav from "./components/Nav"
+import Resume from "./components/Resume";
+import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
 function App() {
+  
+  const [currentPage, setCurrentPage] = useState('About');
 
-  const [currentPage, setCurrentPage] = useState("About");
+  // conditionals to decide the render on the page
 
   const renderPage = () => {
-    if (currentPage === "About") {
+    if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === "Portfolio") {
+    if (currentPage === 'Portfolio') {
       return <Portfolio />;
     }
-    if (currentPage === "Contact") {
+    if (currentPage === 'Contact') {
       return <Contact />;
     }
     return <Resume />;
   };
 
+  // handler for page change with onClick event for any navbar links
   const handlePageChange = (page) => setCurrentPage(page);
-
+  
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
         <Nav
-        currentPage={currentPage}
-        handlePageChange={handlePageChange}>
-        </Nav>
+          className=""
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}>
+          </Nav>
       </header>
       <main>
         {renderPage()}
